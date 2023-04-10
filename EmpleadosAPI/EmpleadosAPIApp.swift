bbeac0f8f42d8767e9fbfa66aa9edaf5a6f4a400
Javier Etxarri <javieretxarrimarin@gmail.com>
2023-04-10 20:01:21 +0200

    Initial Commit

---
+++ EmpleadosAPI/EmpleadosAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmpleadosAPIApp.swift
+//  EmpleadosAPI
+//
+//  Created by Javier Etxarri on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct EmpleadosAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
